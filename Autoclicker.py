24121b4071fadd710b6c3d495475af5907739569
Calle Pålsson <carlerikuno@gmail.com>
2025-03-12 19:27:54 +0100

    Calle updated Autoclicker.py

--- Autoclicker.py
+++ Autoclicker.py
@@ -1,1 +1,14 @@
+import pyautogui
+import time
 
+# Fördröjning innan programmet startar (så du hinner byta fönster)
+time.sleep(5)
+
+print("Autoclicker startad! Tryck CTRL+C för att stoppa.")
+
+try:
+    while True:
+        pyautogui.click()  # Klickar på musen
+        time.sleep(0.1)  # Väntar 0.1 sek mellan klick (ändra om du vill snabbare/långsammare)
+except KeyboardInterrupt:
+    print("\nAutoclicker stoppad!")
